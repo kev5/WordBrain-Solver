0fcaab92b88f3a2605d1b2fd56cc9fb94e7f4f4a
Keval Khara <kevalkhara95@gmail.com>
2017-12-23 14:33:42 -0500

    Update wordbrainsolver.py

--- wordbrainsolver.py
+++ wordbrainsolver.py
@@ -1,250 +1,212 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
-# Copyright 2017 Harish N Sathishchandra harishns@bu.edu
-# Copyright 2017 Junchen Cai cairnedx@bu.edu
 
 '''WordBrain Solver'''
 from sys import argv
 import numpy as np
 
 
-def trie(words):
+class Trie:
     '''Retrieval Data Structure'''
-    word_1 = dict()
-    for word in words:
-        new_word = word_1
+
+    def __init__(self):
+        '''Initialize'''
+        self.head = Node()
+
+    def add(self, word):
+        '''Adding a new string'''
+        curr_node = self.head
         for letter in word:
-            new_word = new_word.setdefault(letter, {})
-        new_word[0] = {}
-    return word_1
+            ind = ord(letter) - 97
+            if curr_node.children[ind]:
+                curr_node = curr_node.children[ind]
+            else:
+                curr_node.add_child(ind)
+                curr_node = curr_node.children[ind]
+        curr_node.data = True
+
+    def check_word(self, word):
+        '''Checks if the word is present'''
+        curr_node = self.head
+        for letter in word:
+            ind = ord(letter) - 97
+            if curr_node.children[ind]:
+                curr_node = curr_node.children[ind]
+        return curr_node.data
+
+    def search(self, letters):
+        '''Checks for the previous letters in the list'''
+        top_node = self.head
+        for letter in letters:
+            ind = ord(letter) - 97
+            if top_node.children[ind]:
+                top_node = top_node.children[ind]
+            else:
+                return False
+        return True
 
 
-def routes(paths, neighbors):
-    '''Different Routes'''
+class Node:
+    '''Class for each Node in the Trie'''
+
+    def __init__(self):
+        '''Initializing'''
+        self.data, self.children = None, np.empty(26, dtype=Node)
+
+    def add_child(self, key):
+        '''Adding a child for each Node'''
+        self.children[key] = Node()
+
+
+def neighbors(matrix, pos):
+    '''Finding Neighbors'''
+    adj = []
+    for i in range(pos[0] - 1, pos[0] + 2):
+        for j in range(pos[1] - 1, pos[1] + 2):
+            if 0 <= i < matrix.shape[0] and 0 <= j < matrix.shape[1]:
+                if matrix[i][j] != '#':
+                    adj.append((i, j))
+    return adj
+
+
+def find_word(prob, pos):
+    '''Search for the word'''
+    for i in range(pos, -1, -1):
+        if prob[i] == ' ':
+            return "".join(prob[i + 1:pos + 1])
+    return "".join(prob[0:pos + 1])
+
+
+def find_pos(matrix, char):
+    '''Find the position of the Letter'''
     result = []
-    for neighbor in neighbors:
-        result.append(paths + [neighbor])
+    for i in range(0, matrix.shape[0]):
+        for j in range(0, matrix.shape[1]):
+            if matrix[i][j] == char:
+                result.append((i, j))
     return result
 
 
-def find_neighbor(index, exist, matrix):
-    '''Finding Neighboring Letters'''
-    locations = []
-    x, y = index
-    nrows, ncols = np.shape(matrix)
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            index = (x + i, y + j)
-            if (index[0] >= 0 and index[1] >= 0
-                and index[0] < nrows):
-                if (index[1] < ncols and index not in exist 
-                    and matrix[index]):
-                    locations.append(index)
-    return locations
+def drop_letters(matrix):
+    '''Drop the used letters in the matrix'''
+    result = np.copy(matrix)
+    previous = ['#'] * matrix.shape[0]
+    for j in range(0, matrix.shape[1]):
+        temp_mat = np.array(previous)
+        index = matrix.shape[0] - 1
+        for i in range(matrix.shape[0] - 1, -1, -1):
+            if result[i][j] != '#':
+                temp_mat[index] = result[i][j]
+                index = index - 1
+        result[:, j] = temp_mat
+    return result
 
 
-def valid_path(path, matrix, word_1):
-    '''Valid Path'''
-    char = ''
-    tmp = word_1
-    for index in path:
-        char = matrix[index]
-        if char not in tmp:
+SMALL_LIST = Trie()
+LARGE_LIST = Trie()
+FINAL = set()
+
+
+class Puzzle:
+    '''Class to represent the puzzles'''
+
+    def __init__(self, matrix, puzzle, matrix_pos, puzz_pos, trie):
+        '''Initialize'''
+        self.matrix, self.puzzle, self.curr_pos, self.puzz_pos, self.trie = \
+            matrix, puzzle, matrix_pos, puzz_pos, trie
+
+    def solve_puzzle(self):
+        '''Solving the Input Text File'''
+        if self.puzz_pos == len(self.puzzle):
+            word = find_word(self.puzzle, self.puzz_pos - 1)
+            if self.trie.check_word(word):
+                FINAL.add("".join(self.puzzle))
             return 0
-        tmp = tmp[char]
-    return 1
-
-
-def find_combinations(index, size, matrix, word_1):
-    '''Finding Combinations'''
-    exist = [index]
-    neibour = find_neighbor(exist[0], exist, matrix)
-    exist = routes(exist, neibour)
-    i = 2
-    if size == 2:
-        combo = exist
-    while i < size:
-        combo = []
-        for path in exist:
-            if valid_path(path, matrix, word_1):
-                co1 = routes(path, find_neighbor(
-                    path[-1], path, matrix))
-                combo += co1
-        exist = combo
-        i += 1
-    return combo
-
-
-def create_word(path, matrix, word_1):
-    '''Create Word from the Path'''
-    word = ''
-    for index in path:
-        word += matrix[index]
-    if not search_word([word], word_1):
-        return 0
-    return word
-
-
-def search_word(key_word_list, word_1):
-    '''Searching for Words'''
-    for word in key_word_list:
-        tmp = word_1
-        for val in word:
-            if val not in tmp:
-                return 0
+        if self.puzzle[self.puzz_pos] == ' ':
+            word = find_word(self.puzzle, self.puzz_pos - 1)
+            if self.trie.check_word(word):
+                rmatrix = drop_letters(self.matrix)
+                Puzzle(rmatrix, self.puzzle, (), self.puzz_pos + 1,
+                       self.trie).solve_puzzle()
+        if self.puzzle[self.puzz_pos] == '*':
+            if self.curr_pos:
+                adj_curr_pos = neighbors(self.matrix, self.curr_pos)
+                if adj_curr_pos:
+                    for pos in adj_curr_pos:
+                        new_puzz = self.puzzle[:]
+                        new_puzz[self.puzz_pos] = self.matrix[pos[0]][pos[1]]
+                        word = find_word(new_puzz, self.puzz_pos)
+                        if self.trie.search(word):
+                            new_matrix = np.copy(self.matrix)
+                            new_matrix[pos[0]][pos[1]] = '#'
+                            Puzzle(new_matrix, new_puzz, (pos[0], pos[1]),
+                                   self.puzz_pos + 1, self.trie).solve_puzzle()
             else:
-                tmp = tmp[val]
-        if 0 in tmp:
-            return 1
-
-
-def remove_word(path, matrix):
-    '''Removing Unwanted Words'''
-    temporary_mat_1 = np.copy(matrix)
-    if len(path) == 1:
-        return matrix
-    num = np.shape(temporary_mat_1)[0]
-    newmatrix1 = []
-    for index in path:
-        temporary_mat_1[index] = '0'
-    for column in np.transpose(temporary_mat_1).tolist():
-        newcolumn1 = list(filter(('0').__ne__, column))
-        while len(newcolumn1) < num:
-            newcolumn1.insert(0, 0)
-        newmatrix1.append(newcolumn1)
-    return np.transpose(np.array(
-        newmatrix1, dtype=object).reshape(num, num))
-
-
-def matrix_words(matrix, length, word_1, blank_space):
-    '''All possible matrices and words'''
-    full_matrix = []
-    all_indices = []
-    if blank_space == []:
-        for i in range(np.shape(matrix)[0]):
-            for j in range(np.shape(matrix)[0]):
-                all_indices.append((i, j))
-    else:
-        all_indices = blank_space
-    for index in all_indices:
-        if matrix[index]:
-            for path in find_combinations(
-                index, length, matrix, word_1):
-                if create_word(path, matrix, word_1):
-                    full_matrix.append((remove_word(path, matrix),
-                                       create_word(
-                                        path, matrix, word_1)))
-    return full_matrix
-
-
-def index_search(blank, sindex_1, matrix):
-    '''Getting indices of blanks'''
-    ind = []
-    for star in blank:
-        if star != '*':
-            for i in range(sindex_1):
-                for j in range(sindex_1):
-                    if matrix[i][j] == star:
-                        ind.append((i, j))
-    return ind
-
-
-def window_slide(matrix, windows, word_1, blank_rows):
-    '''Sliding the window'''
-    sindex_1 = np.shape(matrix)[0]
-    result = []
-    tmp = []
-    ind = []
-    if blank_rows:
-        blank = blank_rows[0]
-        ind = index_search(blank, sindex_1, matrix)
-        for mtx, wrd in matrix_words(matrix, windows[0], word_1, ind):
-            result.append(([wrd], mtx))
-        blank_rows.pop(0)
-        windows.pop(0)
-        if windows:
-            for length in windows:
-                blank = blank_rows[windows.index(length)]
-                ind = index_search(blank, sindex_1, matrix)
-                tmp = result
-                result = []
-                for val in tmp:
-                    for mtx, wrd in matrix_words(val[1], length, 
-                                                 word_1, ind):
-                        result.append((val[0] + [wrd], mtx))
-        return result
-
-
-def get_result(matrix, word_1, column_out_line, blank):
-    '''Getting all Results'''
-    windows = []
-    blank_rows = []
-    for star in blank:
-        blank_rows.append(star)
-    for col in column_out_line:
-        windows.append(col)
-    answers = []
-    result = window_slide(matrix, windows, word_1, blank_rows)
-    for val in result:
-        if val[0] not in answers:
-            answers.append(val[0])
-    for ans in sorted(answers):
-        print(' '.join(ans))
-    return answers
-
-
-def final(line_1, short_word_1, long_word_1, 
-               column_out_line, blank):
-    '''Final Answer'''
-    answer = []
-    answer = get_result(line_1, short_word_1, column_out_line, 
-                        blank)
-    if answer == []:
-        answer = get_result(line_1, long_word_1, column_out_line, 
-                            blank)
-    print('.')
+                for i in range(0, self.matrix.shape[0]):
+                    for j in range(0, self.matrix.shape[1]):
+                        if self.matrix[i][j] != '#':
+                            new_puzz = self.puzzle[:]
+                            new_puzz[self.puzz_pos] = self.matrix[i][j]
+                            word = find_word(new_puzz, self.puzz_pos)
+                            if self.trie.search(word):
+                                new_matrix = np.copy(self.matrix)
+                                new_matrix[i][j] = '#'
+                                Puzzle(new_matrix, new_puzz, (i, j),
+                                       self.puzz_pos + 1,
+                                       self.trie).solve_puzzle()
+        else:
+            if self.curr_pos:
+                adj_curr_pos1 = set(neighbors(self.matrix, self.curr_pos))
+                tmp_pos = set(find_pos(self.matrix,
+                                       self.puzzle[self.puzz_pos]))
+                if adj_curr_pos1 and tmp_pos:
+                    moves = adj_curr_pos1 & tmp_pos
+                    for pos in moves:
+                        new_matrix = np.copy(self.matrix)
+                        new_matrix[pos[0]][pos[1]] = '#'
+                        Puzzle(new_matrix, self.puzzle, (pos[0], pos[1]),
+                               self.puzz_pos + 1, self.trie).solve_puzzle()
+            else:
+                tmp_pos1 = find_pos(self.matrix, self.puzzle[self.puzz_pos])
+                if tmp_pos1:
+                    for pos in tmp_pos1:
+                        new_matrix = np.copy(self.matrix)
+                        new_matrix[pos[0]][pos[1]] = '#'
+                        Puzzle(new_matrix, self.puzzle, (pos[0], pos[1]),
+                               self.puzz_pos + 1, self.trie).solve_puzzle()
 
 
 def main():
-    '''Reading Puzzles and Computing'''
-    with open(argv[1], 'r') as word_list1:
-        words1 = word_list1.read().split()
-    with open(argv[2], 'r') as word_list2:
-        words2 = word_list2.read().split()
+    '''Reading the Input files and Computing Answers'''
+    for line in open(argv[1], 'r'):
+        SMALL_LIST.add(line.rstrip())
+    for line in open(argv[2], 'r'):
+        LARGE_LIST.add(line.rstrip())
+    inputs = []
     while 1:
         try:
-            line_1 = []
-            rows = []
-            blank = []
-            blank_rows = []
-            column_out_line = []
-            while 1:
-                try:
-                    in_puzz = input()
-                    if '*' and ' ' not in in_puzz:
-                        for puzz in in_puzz:
-                            rows.append(puzz)
-                        line_1.append(rows)
-                        rows = []
-                    else:
-                        column_out = 0
-                        for puzz in in_puzz + ' ':
-                            if puzz == ' ':
-                                column_out_line.append(column_out)
-                                column_out = 0
-                                blank_rows.append(blank)
-                                blank = []
-                            else:
-                                column_out += 1
-                                blank.append(puzz)
-                        break
-                except EOFError:
-                    break        
-            line_1, column_out_line, blank = np.array(
-                  line_1), column_out_line, blank_rows
-            final(line_1, trie(words1), trie(words2), 
-                       column_out_line, blank)
-        except TypeError:
+            text = input("")
+        except EOFError:
             break
+        if "*" in text:
+            puzzle = list(text)
+            matrix = np.array(inputs)
+            Puzzle(matrix, puzzle, (), 0, SMALL_LIST).solve_puzzle()
+            if not FINAL:
+                Puzzle(matrix, puzzle, (), 0, LARGE_LIST).solve_puzzle()
+            if FINAL:
+                word_list = list(FINAL)
+                word_list.sort()
+                try:
+                    for string in word_list:
+                        print(string)
+                except ValueError:
+                    pass
+            print(".")
+            FINAL.clear()
+            inputs = []
+        else:
+            inputs.append(list(text))
+
 
 if __name__ == '__main__':
     main()
